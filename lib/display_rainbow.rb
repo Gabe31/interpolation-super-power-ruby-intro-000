237420cb08184635943af47e886dfcfd822b673b
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-27 03:54:38 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,13 +3,13 @@
 def display_rainbow(colors)
   puts "R: #{colors[0]},"
   puts "O: #{colors[1]},"
-  puts "Y: #{colors[3]},"
-  puts "G: #{colors[2]},"
+  puts "Y: #{colors[2]},"
+  puts "G: #{colors[3]},"
   puts "B: #{colors[4]},"
   puts "I: #{colors[5]},"
   puts "V: #{colors[6]},"
 end
 
-color = ['red', 'orange', 'green', 'yellow', 'blue', 'indigo', 'violet']
+color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 puts display_rainbow(color)
